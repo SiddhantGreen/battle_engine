c27c378b28e870aad3d215425e056878a8e32795
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2017-06-30 02:47:44 -0400

    Exp calculation and set functions

---
+++ src/exp_calc/exp_calc.c
@@ -0,0 +1,55 @@
+#include <pokeagb/pokeagb.h>
+#include "../battle_data/pkmn_bank.h"
+#include "../battle_data/pkmn_bank_stats.h"
+#include "../battle_state.h"
+#include "../moves/moves.h"
+#include "../battle_text/battle_pick_message.h"
+
+extern bool enqueue_message(u16 move, u8 bank, enum battle_string_ids id, u16 effect);
+extern void dprintf(const char * str, ...);
+
+u32 calc_exp(u8 fainted, u8 reciever)
+{
+    u8 trainer_mon = (battle_type_flags != BATTLE_FLAG_WILD) ? 150 : 100;
+    // base species recieve incase of species index change
+    u16 base_yield = pokemon_getattr(p_bank[fainted]->this_pkmn, REQUEST_SPECIES, NULL);
+    base_yield = pokemon_base_stats[base_yield].xp_yield;
+    u8 fainted_lvl = p_bank[fainted]->b_data.level;
+    u8 reciever_lvl = p_bank[reciever]->b_data.level;
+    
+    u8 traded = pokemon_getattr(p_bank[reciever]->this_pkmn, REQUEST_TID, NULL);
+    traded = (traded == saveblock2->trainerid) ? 100 : 150;
+    
+    u32 exp_part1 = fainted_lvl * base_yield;
+    exp_part1 = NUM_MOD(exp_part1, trainer_mon);
+    exp_part1 = exp_part1 / 5;
+    dprintf("Current part 1 = %d exp\n", exp_part1);
+    
+    u32 exp_part2 = ((2 *fainted_lvl) + 10);
+    exp_part2 *= (exp_part2 * NUM_MOD(exp_part2, 50));
+    dprintf("Current part 2 = %d exp\n", exp_part2);
+    u32 exp_part3 = (fainted_lvl + reciever_lvl + 10);
+    exp_part3 *= (exp_part3 * NUM_MOD(exp_part3, 50));
+    dprintf("Current part 3 = %d exp\n", exp_part3);
+    exp_part2 /= exp_part3;
+    exp_part2 = MAX(exp_part2, 1);
+    exp_part1 = (exp_part1 * exp_part2) + 1;
+    dprintf("Current %d exp\n", exp_part1);
+    return NUM_MOD(exp_part1, traded);   
+}
+
+/* TODO exp share item - probably as a callback here */
+/* TODO Lucky egg item - probably as a callback here */
+/* TODO Passpower/O-power - probably not implementing this */
+void give_exp(u8 fainted, u8 reciever)
+{
+    u32 exp = calc_exp(fainted, reciever);
+    dprintf("Player gains %d exp\n", exp);
+    enqueue_message(0, reciever, STRING_EXP_GAIN, exp);
+    /* TODO add task that grants exp*/
+    exp += pokemon_getattr(p_bank[reciever]->this_pkmn, REQUEST_EXP_POINTS, NULL);
+    pokemon_setattr(p_bank[reciever]->this_pkmn, REQUEST_EXP_POINTS, &exp);
+    recalculate_stats(p_bank[reciever]->this_pkmn);
+    return;
+}
+
