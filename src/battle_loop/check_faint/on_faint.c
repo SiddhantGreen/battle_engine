0e4c3ace34f7ba627aea35d7e93c5c7ffbf005c6
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2017-07-01 23:00:30 -0400

    renamed file and fixed few bugs

--- src/battle_loop/check_faint/on_faint.c
+++ src/battle_loop/check_faint/on_faint.c
@@ -4,12 +4,13 @@
 #include "../../battle_data/battle_state.h"
 #include "../../moves/moves.h"
 #include "../../battle_text/battle_pick_message.h"
+#include "../move_chain_states.h"
 
 extern bool enqueue_message(u16 move, u8 bank, enum battle_string_ids id, u16 effect);
 extern bool peek_message(void);
 extern void run_decision(void);
 extern void dprintf(const char * str, ...);
-
+extern void give_exp(u8 fainted, u8 reciever);
 
 void obj_battler_fall_through(struct Object* obj)
 {
@@ -55,25 +56,26 @@ void on_faint()
     while (peek_message())
         return;
     switch (super.multi_purpose_state_tracker) {
-        case 0:
+        case S_CHECK_BANK1_FAINT:
             if (!B_CURRENT_HP(battle_master->first_bank))
                 faint(battle_master->first_bank);
-            super.multi_purpose_state_tracker++;
+            super.multi_purpose_state_tracker = S_CHECK_BANK2_FAINT;
             break;
-        case 1:
+        case S_CHECK_BANK2_FAINT:
             if (!B_CURRENT_HP(battle_master->second_bank))
                 faint(battle_master->second_bank);
-            super.multi_purpose_state_tracker++;
+            super.multi_purpose_state_tracker = S_RESOLVE_FAINTS;
             break;
-        case 2:
+        case S_RESOLVE_FAINTS:
             if (p_bank[PLAYER_SINGLES_BANK]->b_data.fainted) {
-                super.multi_purpose_state_tracker = 7;
+                // player has lost, exit battle TODO: Teleport to PC & whiteout checks. Else switch.
+                super.multi_purpose_state_tracker = S_END_BATTLE;
             } else if (p_bank[OPPONENT_SINGLES_BANK]->b_data.fainted) {
                 // player wins, give exp
                 give_exp(OPPONENT_SINGLES_BANK, PLAYER_SINGLES_BANK);
-                super.multi_purpose_state_tracker = 7;
+                super.multi_purpose_state_tracker = S_END_BATTLE;
             } else {
-                super.multi_purpose_state_tracker = 5; // exit
+                super.multi_purpose_state_tracker = S_RUN_MOVE_ALTERNATE_BANK;
                 
             }
             set_callback1(run_decision);
