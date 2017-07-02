fbec304595c030aa99f265b95ca7dd6909dfc8fd
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2017-07-01 23:01:49 -0400

    File for state tracker enums

---
+++ src/battle_loop/move_chain_states.h
@@ -0,0 +1,32 @@
+#ifndef MOVE_CHAIN_STATES_H_
+#define MOVE_CHAIN_STATES_H_
+
+enum RunDecisionStates {
+    S_RUN_SWITCH = 0,
+    S_RUN_AFTER_SWITCH,
+    S_RUN_SWITCH_ALTERNATE_BANK,
+    S_RUN_MOVE,
+    S_RUN_FAINT,
+    S_RUN_MOVE_ALTERNATE_BANK,
+    S_SOFT_RESET_BANK,
+    S_END_BATTLE,
+    
+};
+
+enum RunFaintStates {
+    S_CHECK_BANK1_FAINT = 0,
+    S_CHECK_BANK2_FAINT,
+    S_RESOLVE_FAINTS,
+};
+
+
+enum RunFleeStates {
+    S_CHECK_FLEEING = 0,
+    S_NOT_FLEEING,
+    S_TRY_FLEE,
+    
+};
+
+
+#endif /* MOVE_CHAIN_STATES_H_ */
+
