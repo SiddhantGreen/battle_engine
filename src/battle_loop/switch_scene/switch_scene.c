3922cb80520d9f20f8de47bc37d15b27c87f475a
SBird1337 <auerphilipp38@yahoo.de>
2017-07-07 00:54:09 +0200

    nature colorized

--- src/battle_loop/switch_scene/switch_scene.c
+++ src/battle_loop/switch_scene/switch_scene.c
@@ -8,6 +8,9 @@
 
 #define OBJID_HIDE(objid) objects[objid].final_oam.affine_mode = 2
 #define rgb5(r, g, b) (u16)((r >> 3) | ((g >> 3) << 5) | ((b >> 3) << 10))
+#define STAT_COLOR(stat, pkmn)                                                                                         \
+    (nature_stat_boosted(stat, pkmn) ? &switch_color_green                                                             \
+                                     : (nature_stat_nerved(stat, pkmn) ? &switch_color_red : &switch_color))
 
 #define SWB_ABILITY 0
 #define SWB_ITEM 1
@@ -28,13 +31,10 @@ extern u8 load_dmg_type_icon(u8 type, s16 x, s16 y, u8 tag);
 
 static const pchar str_no_item[] = _("None");
 
-static u16 switch_text_pal[] = {
-    rgb5(255, 0, 255),  rgb5(248, 248, 248), rgb5(112, 112, 112),
-    rgb5(96, 96, 96),   rgb5(208, 208, 208), rgb5(76, 154, 38),
-    rgb5(102, 194, 66), rgb5(168, 75, 76),   rgb5(224, 114, 75),
-    rgb5(180, 124, 41), rgb5(241, 188, 60),  rgb5(255, 0, 255),
-    rgb5(255, 0, 255),  rgb5(255, 0, 255),   rgb5(255, 0, 255),
-    rgb5(255, 0, 255)};
+static u16 switch_text_pal[] = {rgb5(255, 0, 255),   rgb5(248, 248, 248), rgb5(112, 112, 112), rgb5(96, 96, 96),
+                                rgb5(208, 208, 208), rgb5(76, 154, 38),   rgb5(102, 194, 66),  rgb5(168, 75, 76),
+                                rgb5(224, 114, 75),  rgb5(180, 124, 41),  rgb5(241, 188, 60),  rgb5(255, 0, 255),
+                                rgb5(255, 0, 255),   rgb5(255, 0, 255),   rgb5(255, 0, 255),   rgb5(255, 0, 255)};
 
 static struct TextColor switch_color = {0, 3, 4};
 static struct TextColor switch_color_bg = {0, 1, 2};
@@ -200,8 +200,7 @@ void switch_setup(void) {
                                   .map_base = 28,
                                   .character_base = 3,
                                   .bgid = 3};
-    struct BgConfig bg_config_data[4] = {bg0_config, bg1_config, bg2_config,
-                                         bg3_config};
+    struct BgConfig bg_config_data[4] = {bg0_config, bg1_config, bg2_config, bg3_config};
     bgid_mod_x_offset(0, 0, 0);
     bgid_mod_y_offset(0, 0, 0);
     bgid_mod_x_offset(1, 0, 0);
@@ -244,8 +243,7 @@ void switch_load_background(void) {
 }
 
 void switch_type_update_icon(u8 objid, enum MoveTypes type) {
-    void *vram =
-        (void *)((0x06010000) + objects[objid].final_oam.tile_num * 32);
+    void *vram = (void *)((0x06010000) + objects[objid].final_oam.tile_num * 32);
     memcpy(vram, (type * 256) + type_iconsTiles, 256);
 }
 
@@ -253,9 +251,22 @@ void switch_type_icon_load(u8 type, s16 x, s16 y, u8 id) {
     if (battle_master->switch_objid[id] != 0x3F) {
         switch_type_update_icon(battle_master->switch_objid[0], type);
     } else {
-        battle_master->switch_objid[id] =
-            load_dmg_type_icon(type, x, y, id + 4);
+        battle_master->switch_objid[id] = load_dmg_type_icon(type, x, y, id + 4);
     }
+}
+
+bool nature_stat_boosted(u8 stat, struct Pokemon *pokemon) {
+    u8 nature = get_nature(pokemon_getattr(pokemon, REQUEST_PID, NULL));
+    if ((nature / 5) == (nature % 5))
+        return false;
+    return stat == (nature / 5);
+}
+
+bool nature_stat_nerved(u8 stat, struct Pokemon *pokemon) {
+    u8 nature = get_nature(pokemon_getattr(pokemon, REQUEST_PID, NULL));
+    if ((nature / 5) == (nature % 5))
+        return false;
+    return stat == (nature % 5);
 }
 
 void switch_load_pokemon_data(struct Pokemon *pokemon) {
@@ -274,30 +285,25 @@ void switch_load_pokemon_data(struct Pokemon *pokemon) {
     rboxid_print(SWB_NAME, 0, 0, 4, &switch_color_bg, 0, &string_buffer[0]);
 
     /* print the stats */
-    fmt_int_10(string_buffer, pokemon_getattr(pokemon, REQUEST_ATK, NULL), 0,
-               4);
-    rboxid_print(SWB_ATK, 0, 1, 7, &switch_color, 0, &string_buffer[0]);
 
-    fmt_int_10(string_buffer, pokemon_getattr(pokemon, REQUEST_DEF, NULL), 0,
-               4);
-    rboxid_print(SWB_DEF, 0, 2, 7, &switch_color, 0, &string_buffer[0]);
+    fmt_int_10(string_buffer, pokemon_getattr(pokemon, REQUEST_ATK, NULL), 0, 4);
+    rboxid_print(SWB_ATK, 0, 1, 7, STAT_COLOR(0, pokemon), 0, &string_buffer[0]);
 
-    fmt_int_10(string_buffer, pokemon_getattr(pokemon, REQUEST_SPATK, NULL), 0,
-               4);
-    rboxid_print(SWB_SPA, 0, 3, 7, &switch_color, 0, &string_buffer[0]);
+    fmt_int_10(string_buffer, pokemon_getattr(pokemon, REQUEST_DEF, NULL), 0, 4);
+    rboxid_print(SWB_DEF, 0, 2, 7, STAT_COLOR(1, pokemon), 0, &string_buffer[0]);
 
-    fmt_int_10(string_buffer, pokemon_getattr(pokemon, REQUEST_SPDEF, NULL), 0,
-               4);
-    rboxid_print(SWB_SPD, 0, 4, 7, &switch_color, 0, &string_buffer[0]);
+    fmt_int_10(string_buffer, pokemon_getattr(pokemon, REQUEST_SPATK, NULL), 0, 4);
+    rboxid_print(SWB_SPA, 0, 3, 7, STAT_COLOR(3, pokemon), 0, &string_buffer[0]);
 
-    fmt_int_10(string_buffer, pokemon_getattr(pokemon, REQUEST_SPD, NULL), 0,
-               4);
-    rboxid_print(SWB_SPE, 0, 5, 7, &switch_color, 0, &string_buffer[0]);
+    fmt_int_10(string_buffer, pokemon_getattr(pokemon, REQUEST_SPDEF, NULL), 0, 4);
+    rboxid_print(SWB_SPD, 0, 4, 7, STAT_COLOR(4, pokemon), 0, &string_buffer[0]);
+
+    fmt_int_10(string_buffer, pokemon_getattr(pokemon, REQUEST_SPD, NULL), 0, 4);
+    rboxid_print(SWB_SPE, 0, 5, 7, STAT_COLOR(2, pokemon), 0, &string_buffer[0]);
 
     /* print the ability */
 
-    rboxid_print(SWB_ABILITY, 0, 3, 1, &switch_color, 0,
-                 &pokemon_ability_names[get_ability(pokemon)][0]);
+    rboxid_print(SWB_ABILITY, 0, 3, 1, &switch_color, 0, &pokemon_ability_names[get_ability(pokemon)][0]);
 
     /* print the item */
     /* TODO: check on that item, there may be an internal routine for it */
@@ -312,21 +318,20 @@ void switch_load_pokemon_data(struct Pokemon *pokemon) {
         u8 move = pokemon_getattr(pokemon, REQUEST_MOVE1 + i, NULL);
         if (move == 0)
             continue;
-        rboxid_print(SWB_MOVES, 0, 0, (4 + 14 * i), &switch_color, 0,
-                     &moves[move].name[0]);
+        rboxid_print(SWB_MOVES, 0, 0, (4 + 14 * i), &switch_color, 0, &moves[move].name[0]);
 
         fmt_int_10(string_buffer, moves[move].base_power, 0, 4);
-        rboxid_print(SWB_POW, 0, 1, (4 + 14 * i), &switch_color, 0,
-                     &string_buffer[0]);
+        rboxid_print(SWB_POW, 0, 1, (4 + 14 * i), &switch_color, 0, &string_buffer[0]);
 
         fmt_int_10(string_buffer, moves[move].accuracy, 0, 4);
-        rboxid_print(SWB_ACC, 0, 5, (4 + 14 * i), &switch_color, 0,
-                     &string_buffer[0]);
+        rboxid_print(SWB_ACC, 0, 5, (4 + 14 * i), &switch_color, 0, &string_buffer[0]);
 
         u8 pp = pokemon_getattr(pokemon, REQUEST_PP1 + i, NULL);
         fmt_int_10(string_buffer, pp, 0, 4);
-        rboxid_print(SWB_PP, 0, 2, (4 + 14 * i), &switch_color, 0,
-                     &string_buffer[0]);
+        rboxid_print(SWB_PP, 0, 2, (4 + 14 * i), &switch_color, 0, &string_buffer[0]);
+
+        /*the move type icon*/
+        switch_type_icon_load(moves[move].type, 49, 84 + (14 * i), 2 + i);
     }
 
     for (u32 i = SWB_ABILITY; i <= SWB_NAME; ++i) {
@@ -341,10 +346,10 @@ void switch_load_pokemon_data(struct Pokemon *pokemon) {
 
     /* load the type icons */
     if ((enum MoveTypes)(pokemon_base_stats[species].type[0]) != MTYPE_EGG) {
-        switch_type_icon_load(pokemon_base_stats[species].type[0], 48, 25, 0);
+        switch_type_icon_load(pokemon_base_stats[species].type[0], 47, 25, 0);
     }
     if ((enum MoveTypes)(pokemon_base_stats[species].type[1]) != MTYPE_EGG) {
-        switch_type_icon_load(pokemon_base_stats[species].type[1], 84, 25, 1);
+        switch_type_icon_load(pokemon_base_stats[species].type[1], 83, 25, 1);
     }
 }
 
