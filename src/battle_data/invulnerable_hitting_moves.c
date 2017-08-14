b1de8c9d498b07b0a57802c98f482d34e48d3629
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2017-08-13 21:12:12 -0400

    Bug fix

--- src/battle_data/invulnerable_hitting_moves.c
+++ src/battle_data/invulnerable_hitting_moves.c
@@ -5,8 +5,8 @@
 
 extern void dprintf(const char * str, ...);
 
-u8 gust_on_modify(u8 user, u8 target, u16 move)
-    if ((CURRENT_MOVE(TARGET) == MOVE_BOUNCE) || (CURRENT_MOVE(TARGET) == MOVE_FLY)) {
+u8 gust_on_modify(u8 user, u8 target, u16 move) {
+    if ((CURRENT_MOVE(target) == MOVE_BOUNCE) || (CURRENT_MOVE(target) == MOVE_FLY)) {
         if (HAS_VOLATILE(target, VOLATILE_SEMI_INVULNERABLE)) {
             B_MOVE_POWER(user) *= 2;
         }
