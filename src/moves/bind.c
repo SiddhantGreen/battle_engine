8d4227c90def7933e97c7375f4bf6ea437547e9e
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2017-11-11 22:30:32 -0500

    bind move finished

--- src/moves/bind.c
+++ src/moves/bind.c
@@ -3,23 +3,25 @@
 #include "../battle_data/pkmn_bank_stats.h"
 #include "../battle_data/battle_state.h"
 #include "../battle_loop/move_chain_states.h"
+#include "../volatile/residual_effects.h"
 
 extern void dprintf(const char * str, ...);
+extern u8 add_residual_cb(ResidualFunc to_add);
 extern void run_decision(void);
+extern u16 rand_range(u16 min, u16 max);
+extern void apply_residual_dmg(u8 id);
+
 
 u8 bind_on_effect_cb(u8 attacker, u8 defender, u16 move)
 {
     ADD_VOLATILE(defender, VOLATILE_BIND);
+    u8 id = add_residual_cb(apply_residual_dmg);
+    residual_callbacks[id].effective_bank = defender;
+    residual_callbacks[id].active_counter = rand_range(4, 6);
+    residual_callbacks[id].move_id = MOVE_BIND;
+    residual_callbacks[id].status = VOLATILE_BIND;
+    residual_callbacks[id].hp_delta = MAX(1, TOTAL_HP(defender) / 8);
     return 1;
 }
-
-/*
-bool bind_volatile_dmg(u8 bank)
-{
-    // TODO: how can residual effects be implemented better? Does is suffice to check volatiles when needed?
-    u16 hp_to_lose = TOTAL_HP(attacker) / 8;
-    hp_anim_change(bank, MIN(CURRENT_HP(bank), hp_to_lose));
-    enqueue_message(CURRENT_MOVE(bank_index), bank_index, STRING_RECOIL, 0);
-}*/
 
 
