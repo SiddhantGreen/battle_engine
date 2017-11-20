2656078d22f12b0c32a75d11606d355d6f4fd318
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2017-11-20 16:23:50 -0500

    Untyped moves accounted for in dmg calculator
    Untyped moves gain no type effectiveness

--- src/battle_calcs/damage_calc.c
+++ src/battle_calcs/damage_calc.c
@@ -22,6 +22,8 @@ u16 type_effectiveness_mod(u8 attacker, u8 defender, u16 move)
             if ((B_MOVE_TYPE(attacker, i) != MTYPE_EGG) && (B_PKMN_TYPE(defender, j) != MTYPE_EGG)) {
                 u8 target_type = B_PKMN_TYPE(defender, j);
                 u8 move_type = B_MOVE_TYPE(attacker, i);
+				if (move_type == MTYPE_NONE)
+					return 100;
                 u16 move_effectiveness = MOVE_EFFECTIVENESS(target_type, move_type);
                 if (move_effectiveness > 0) {
                     percent = NUM_MOD(percent, move_effectiveness);
