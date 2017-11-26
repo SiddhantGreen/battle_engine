094c0fcac970416a57195e545fb0b2f4db840f93
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2017-11-25 23:01:23 -0500

    Anon callback add

--- src/anonymous_callbacks/anonymous.c
+++ src/anonymous_callbacks/anonymous.c
@@ -1,16 +1,59 @@
 #include <pokeagb/pokeagb.h>
 #include "../moves/moves.h"
 
+// set anon callback for
+bool add_anon_cb(u8 CB_id, s8 priority, u8 delay, u8 dur, u8 src, void* func)
+{
+  struct anonymous_callback* a_cb = (struct anonymous_callback*)malloc_and_clear(sizeof(struct anonymous_callback));
+  struct anonymous_callback* a_cb_start;
 
+  a_cb->priority = priority;
+  a_cb->delay_before_effect = delay;
+  a_cb->duration = dur;
+  a_cb->source_bank = src;
+  a_cb->func = func;
 
+  switch (CB_id) {
+    case CB_ON_BEFORE_TURN:
+      a_cb_start = &(battle_master->anon_cbs.before_turn[0]);
+      break;
+    case CB_ON_BEFORE_SWITCH:
+      a_cb_start = &(battle_master->anon_cbs.before_switch[0]);
+      break;
+    case CB_ON_BEFORE_MOVE:
+      a_cb_start = &(battle_master->anon_cbs.before_move[0]);
+      break;
+    case CB_ON_MOVE_FAIL:
+      a_cb_start = &(battle_master->anon_cbs.on_move_fail[0]);
+      break;
+    case CB_ON_MODIFY_MOVE:
+      a_cb_start = &(battle_master->anon_cbs.on_modify_move[0]);
+      break;
+    case CB_ON_TRYHIT_MOVE:
+      a_cb_start = &(battle_master->anon_cbs.on_tryhit_move[0]);
+      break;
+    case CB_ON_BASE_POWER_MOVE:
+      a_cb_start = &(battle_master->anon_cbs.on_base_power_move[0]);
+      break;
+    case CB_ON_DAMAGE_MOVE:
+      a_cb_start = &(battle_master->anon_cbs.on_damage_move[0]);
+      break;
+    case CB_ON_EFFECT:
+      a_cb_start = &(battle_master->anon_cbs.on_effect_cb[0]);
+      break;
+    case CB_ON_AFTER_MOVE:
+      a_cb_start = &(battle_master->anon_cbs.on_after_move[0]);
+      break;
+    default:
+      return false;
+  };
 
-
-
-
-
-
-
-
-
-
-
+  for (u8 i = 0; i < 4; i++) {
+    if (a_cb_start == NULL) {
+      a_cb_start = a_cb;
+      return true;
+    }
+    a_cb_start++;
+  }
+  return false;
+}
--- src/anonymous_callbacks/anonymous_callbacks.h
+++ src/anonymous_callbacks/anonymous_callbacks.h
@@ -1,24 +1,25 @@
 #include <pokeagb/pokeagb.h>
 #include "../moves/moves.h"
 
-/*
-#define CB_BEFORE_TURN 1
-#define CB_BEFORE_TURN 2
-#define CB_BEFORE_MOVE 3
-#define CB_BEFORE_MOVE 4
-#define CB_BEFORE_MOVE 5
-#define CB_BEFORE_MOVE 6
-#define CB_BEFORE_MOVE 7
-#define CB_BEFORE_MOVE 8
-#define CB_BEFORE_MOVE 9
-#define CB_BEFORE_MOVE 10
-#define CB_BEFORE_MOVE 11*/
+
+#define CB_ON_BEFORE_TURN 1
+#define CB_ON_BEFORE_SWITCH 2
+#define CB_ON_BEFORE_MOVE 3
+#define CB_ON_MOVE_FAIL 4
+#define CB_ON_MODIFY_MOVE 5
+#define CB_ON_TRYHIT_MOVE 6
+#define CB_ON_BASE_POWER_MOVE 7
+#define CB_ON_DAMAGE_MOVE 8
+#define CB_ON_EFFECT 9
+#define CB_ON_AFTER_MOVE 10
+#define CB_BEFORE_MOVE 11
 
 
 struct anonymous_callback {
 	s8 priority;
 	u8 delay_before_effect;
 	u8 duration;
+	u8 source_bank; // source bank using the effect
 	void* data_ptr;
 	void* func;
 };
@@ -27,7 +28,6 @@ struct anonymous_callback {
 struct anonymous_callbacks_master {
 	struct anonymous_callback before_turn[4];
 	struct anonymous_callback before_switch[4];
-	struct anonymous_callback on_start[4];
 	struct anonymous_callback before_move[4];
 	struct anonymous_callback on_move_fail[4];
 	struct anonymous_callback on_modify_move[4];
@@ -37,15 +37,3 @@ struct anonymous_callbacks_master {
 	struct anonymous_callback on_effect_cb[4];
 	struct anonymous_callback on_after_move[4];
 };
-
-
-
-
-
-
-
-
-
-
-
-
