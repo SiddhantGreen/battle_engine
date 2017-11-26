79a4f82a47422278690c0d614b2f0ffd736eb9bb
Adrevi <oa.ummels@outlook.com>
2017-11-27 00:01:53 +0100

    Merge conflict fix
    Thanks for helping blah.

--- src/test_data.c
+++ src/test_data.c
@@ -7,15 +7,20 @@ extern void dprintf(const char * str, ...);
 void set_player_battle_components()
 {
     /* Change these variables to change what the player's first pokemon has */
-    u16 species = SPECIES_SNORLAX;
+	u16 species = SPECIES_CATERPIE;
     //u16 species = pokemon_getattr(&party_player[0], REQUEST_SPECIES, NULL);
-    u16 move_1 = MOVE_RECOVER;
-    u16 move_2 = MOVE_BARRIER;
-    u16 move_3 = MOVE_EGG_BOMB;
-    u16 move_4 = MOVE_LICK;
+    u16 move_1 = MOVE_SNATCH;
+    u16 move_2 = MOVE_EMBER;
+    u16 move_3 = MOVE_FLAME_WHEEL;
+    u16 move_4 = MOVE_POISON_POWDER;
+    u8 level = 34;
 
     pokemon_setattr(&party_player[0], REQUEST_SPECIES, &species);
     pokemon_setattr(&party_player[0], REQUEST_NICK, &pokemon_names[species]);
+    u32 species_exp_index = (pokemon_base_stats[species].exp_growth * 0x194);
+    u32 *exp_needed = (u32*) (0x8253AE4 + (species_exp_index + (level * 4)));
+    pokemon_setattr(&party_player[0], REQUEST_EXP_POINTS, exp_needed);
+    recalculate_stats(&party_player[0]);
     pokemon_setattr(&party_player[0], REQUEST_MOVE1, &move_1);
     pokemon_setattr(&party_player[0], REQUEST_MOVE2, &move_2);
     pokemon_setattr(&party_player[0], REQUEST_MOVE3, &move_3);
@@ -30,15 +35,29 @@ void set_player_battle_components()
 void set_opponent_battle_components()
 {
     /* Change these variables to change what the player's first pokemon has */
+<<<<<<< HEAD
     u16 species = SPECIES_STEELIX;
     //u16 species = pokemon_getattr(&party_opponent[0], REQUEST_SPECIES, NULL);
     u16 move_1 = MOVE_SMOG;
     u16 move_2 = MOVE_SLUDGE;
     u16 move_3 = MOVE_BONE_CLUB;
     u16 move_4 = MOVE_FIRE_BLAST;
+=======
+    u16 species = SPECIES_SQUIRTLE;
+    //u16 species = pokemon_getattr(&party_opponent[0], REQUEST_SPECIES, NULL);
+    u16 move_1 = MOVE_EMBER;
+    u16 move_2 = MOVE_TAIL_GLOW;
+    u16 move_3 = MOVE_HOWL;
+    u16 move_4 = MOVE_IRON_DEFENSE;
+    u8 level = 27;
+>>>>>>> upstream/master
 
     pokemon_setattr(&party_opponent[0], REQUEST_SPECIES, &species);
     pokemon_setattr(&party_opponent[0], REQUEST_NICK, &pokemon_names[species]);
+    u32 species_exp_index = (pokemon_base_stats[species].exp_growth * 0x194);
+    u32 *exp_needed = (u32*) (0x8253AE4 + (species_exp_index + (level * 4)));
+    pokemon_setattr(&party_opponent[0], REQUEST_EXP_POINTS, exp_needed);
+    recalculate_stats(&party_opponent[0]);
     pokemon_setattr(&party_opponent[0], REQUEST_MOVE1, &move_1);
     pokemon_setattr(&party_opponent[0], REQUEST_MOVE2, &move_2);
     pokemon_setattr(&party_opponent[0], REQUEST_MOVE3, &move_3);
