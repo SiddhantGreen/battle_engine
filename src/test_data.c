714430daf87c89e221e7cd6486705c739da833bc
Adrevi <oa.ummels@outlook.com>
2017-11-26 23:51:20 +0100

    Added 15 moves
    Screech
    Double Team
    Recover
    Harden
    Smokescreen
    Confuse Ray
    Withdraw
    Defense Curl
    Barrier
    Egg Bomb
    Lick
    Smog
    Sludge
    Bone Club
    Fire Blast
    
    Also found out about some less-than-100% accuracy bug that was fixed off-screen. Also, recover heals 50 hp instead of 50% but that's also fixed off-screen apparently.
    
    Did some testing but may have missed something, sorry.

--- src/test_data.c
+++ src/test_data.c
@@ -7,12 +7,12 @@ extern void dprintf(const char * str, ...);
 void set_player_battle_components()
 {
     /* Change these variables to change what the player's first pokemon has */
-    u16 species = SPECIES_CHARIZARD;
+    u16 species = SPECIES_SNORLAX;
     //u16 species = pokemon_getattr(&party_player[0], REQUEST_SPECIES, NULL);
-    u16 move_1 = MOVE_SNATCH;
-    u16 move_2 = MOVE_EMBER;
-    u16 move_3 = MOVE_FLAME_WHEEL;
-    u16 move_4 = MOVE_POISON_POWDER;
+    u16 move_1 = MOVE_RECOVER;
+    u16 move_2 = MOVE_BARRIER;
+    u16 move_3 = MOVE_EGG_BOMB;
+    u16 move_4 = MOVE_LICK;
 
     pokemon_setattr(&party_player[0], REQUEST_SPECIES, &species);
     pokemon_setattr(&party_player[0], REQUEST_NICK, &pokemon_names[species]);
@@ -30,12 +30,12 @@ void set_player_battle_components()
 void set_opponent_battle_components()
 {
     /* Change these variables to change what the player's first pokemon has */
-    u16 species = SPECIES_BULBASAUR;
+    u16 species = SPECIES_STEELIX;
     //u16 species = pokemon_getattr(&party_opponent[0], REQUEST_SPECIES, NULL);
-    u16 move_1 = MOVE_EMBER;
-    u16 move_2 = MOVE_TAIL_GLOW;
-    u16 move_3 = MOVE_HOWL;
-    u16 move_4 = MOVE_IRON_DEFENSE;
+    u16 move_1 = MOVE_SMOG;
+    u16 move_2 = MOVE_SLUDGE;
+    u16 move_3 = MOVE_BONE_CLUB;
+    u16 move_4 = MOVE_FIRE_BLAST;
 
     pokemon_setattr(&party_opponent[0], REQUEST_SPECIES, &species);
     pokemon_setattr(&party_opponent[0], REQUEST_NICK, &pokemon_names[species]);
