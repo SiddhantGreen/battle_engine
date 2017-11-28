37b2f77cb164d49f993becd4f68921ba51456ca4
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2017-11-28 02:25:08 -0500

    Complete revamp of callback system
    Almost all callbacks are now anonymous.

--- src/anonymous_callbacks/anonymous.c
+++ src/anonymous_callbacks/anonymous.c
@@ -1,103 +1,128 @@
 #include <pokeagb/pokeagb.h>
 #include "../moves/moves.h"
+#include "../battle_data/battle_state.h"
 
 extern void dprintf(const char * str, ...);
+extern void sort_priority_cbs(void);
 
-struct anonymous_callback* get_anonymous_callback(u8 CB_id) {
-  struct anonymous_callback* a_cb_start = NULL;
-  switch (CB_id) {
-    case CB_ON_BEFORE_TURN:
-      a_cb_start = battle_master->anon_cbs.before_turn;
-      break;
-    case CB_ON_BEFORE_SWITCH:
-      a_cb_start = battle_master->anon_cbs.before_switch;
-      break;
-    case CB_ON_BEFORE_MOVE:
-      a_cb_start = battle_master->anon_cbs.before_move;
-      break;
-    case CB_ON_MOVE_FAIL:
-      a_cb_start = battle_master->anon_cbs.on_move_fail;
-      break;
-    case CB_ON_MODIFY_MOVE:
-      a_cb_start = battle_master->anon_cbs.on_modify_move;
-      break;
-    case CB_ON_TRYHIT_MOVE:
-      a_cb_start = battle_master->anon_cbs.on_tryhit_move;
-      break;
-    case CB_ON_BASE_POWER_MOVE:
-      a_cb_start = battle_master->anon_cbs.on_base_power_move;
-      break;
-    case CB_ON_DAMAGE_MOVE:
-      a_cb_start = battle_master->anon_cbs.on_damage_move;
-      break;
-    case CB_ON_EFFECT:
-      a_cb_start = battle_master->anon_cbs.on_effect_cb;
-      break;
-    case CB_ON_AFTER_MOVE:
-      a_cb_start = battle_master->anon_cbs.on_after_move;
-      break;
-  };
-  return a_cb_start;
+// insert a new anonymous callback
+void add_callback(u8 CB_id, s8 priority, u8 dur, u8 src, u32 func)
+{
+    for (u8 i = 0; i < ANON_CB_MAX; i++) {
+        if (!(CB_MASTER[i].in_use)) {
+            // initialize new callback here
+            CB_MASTER[i].priority = priority;
+            CB_MASTER[i].delay_before_effect = 0;
+            CB_MASTER[i].in_use = true;
+            CB_MASTER[i].duration = dur;
+            CB_MASTER[i].source_bank = src;
+            CB_MASTER[i].cb_id = CB_id;
+            CB_MASTER[i].data_ptr = NULL;
+            CB_MASTER[i].func = func;
+            break;
+        }
+    }
 }
 
-// set anon callback for
-bool add_anon_cb(u8 CB_id, s8 priority, u8 delay, u8 dur, u8 src, u32 func)
-{
-  struct anonymous_callback* a_cb_start = get_anonymous_callback(CB_id);
-  if (a_cb_start) {
-    for (u8 i = 0; i < 4; i++) {
-      if (!a_cb_start[i].in_use) {
-        a_cb_start[i].priority = priority;
-        a_cb_start[i].delay_before_effect = delay;
-        a_cb_start[i].duration = dur;
-        a_cb_start[i].source_bank = src;
-        a_cb_start[i].func = func;
-        a_cb_start[i].in_use = true;
-        return true;
-      }
+// execution order building before executing a specific type of CB
+void build_execution_order(u8 CB_id) {
+    sort_priority_cbs();
+    CB_EXEC_INDEX = 0;
+    for (u8 i = 0; i < ANON_CB_MAX; i++) {
+        if (CB_MASTER[i].cb_id == CB_id) {
+            CB_EXEC_ORDER[CB_EXEC_INDEX] = i;
+            CB_EXEC_INDEX++;
+        }
     }
-    return false; // full
-  } else {
-    return false; // invalid CB_id
-  }
-}
-
-
-u8 exec_anonymous_callback(u8 CB_id, u8 attacker, u8 defender, u16 move)
-{
-  struct anonymous_callback* a_cb_start = get_anonymous_callback(CB_id);
-  if (!a_cb_start->in_use) {
-    return false; // invalid CB id
-  }
-  // sort by priority
-  for (u8 i = 0; i < 4; i++) {
-    if (!a_cb_start[i].in_use)
-      break;
-    for (u8 j = i + 1; j < 4; j++) {
-      if (!a_cb_start[j].in_use)
-        break;
-      if (a_cb_start[i].priority > a_cb_start[j].priority) {
-        struct anonymous_callback temp = a_cb_start[i];
-        a_cb_start[i] = a_cb_start[j];
-        a_cb_start[j] = temp;
-      }
-    }
-  }
-  // execute in order
-  for (u8 i = 0; i < 4; i++) {
-    if (!a_cb_start[i].in_use)
-      return false;
-    AnonymousCallback func = (AnonymousCallback)a_cb_start[i].func;
-    if (a_cb_start[i].delay_before_effect < 1) {
-      if (a_cb_start[i].duration == 0) {
-        a_cb_start[i].in_use = false;
-      } else {
-        a_cb_start[i].duration--;
-      }
+    CB_EXEC_ORDER[CB_EXEC_INDEX] = ANON_CB_MAX;
+    if (CB_EXEC_INDEX) {
+        CB_EXEC_INDEX = 0;
+        dprintf("Some callback was queued at ID: %d\n", CB_id);
     } else {
-      a_cb_start[i].delay_before_effect--;
+        CB_EXEC_INDEX = ANON_CB_MAX;
+
+        dprintf("No callback at ID: %d\n", CB_id);
     }
-    return func(attacker, a_cb_start[i].source_bank, move, &a_cb_start[i]);
-  }
-  return false;
+}
+
+// Run the next callback in the built list
+u16 pop_callback(u8 attacker, u16 move) {
+    // respect delays. Delayed CBs not executed until after delay
+    for (u8 i = CB_EXEC_INDEX; i < ANON_CB_MAX; i++) {
+        if (CB_MASTER[CB_EXEC_ORDER[i]].delay_before_effect)
+            continue;
+        if (CB_MASTER[i].in_use == false) {
+            continue;
+        } else {
+            CB_EXEC_INDEX++;
+            i = CB_EXEC_ORDER[i];
+            AnonymousCallback func = (AnonymousCallback)CB_MASTER[i].func;
+            battle_master->executing = true;
+            return func(attacker, CB_MASTER[i].source_bank, move, &CB_MASTER[i]);
+        }
+    }
+    // callbacks are done executing here
+    battle_master->executing = false;
+    CB_EXEC_INDEX = 0;
+    return true;
+}
+
+
+// turn end, drop counters of array elements
+void update_callbacks() {
+    for (u8 i = 0; i < ANON_CB_MAX; i++) {
+        // Count down turn delay
+        if (!CB_MASTER[i].in_use) continue;
+        if (CB_MASTER[i].delay_before_effect) {
+            CB_MASTER[i].delay_before_effect--;
+        } else {
+            // Count down duration if delay done
+            if (CB_MASTER[i].duration) {
+                CB_MASTER[i].duration--;
+            } else {
+                dprintf("CB id %d marked unused\n", i);
+                CB_MASTER[i].in_use = false;
+                dprintf("CB id %d marked to:%d\n", i, CB_MASTER[i].in_use);
+            }
+        }
+    }
+    sort_priority_cbs();
+}
+
+
+/* Array sorting by struct element Priority */
+void callbacks_swap(u8 i, u8 j)
+{
+    struct anonymous_callback temp;
+    memcpy(&temp, &CB_MASTER[i], sizeof(struct anonymous_callback));
+    memcpy(&CB_MASTER[i], &CB_MASTER[j], sizeof(struct anonymous_callback));
+    memcpy(&CB_MASTER[j], &CB_MASTER[i], sizeof(struct anonymous_callback));
+    //CB_MASTER[i] = CB_MASTER[j];
+    //CB_MASTER[j] = temp;
+    return;
+}
+
+void sort_priority_cbs()
+{
+    for (u8 i = 0; i < ANON_CB_MAX; i++) {
+        for (u8 j = i + 1; j < ANON_CB_MAX; j++) {
+            if (ELE_NULL(i) && ELE_USED(j)) {
+                callbacks_swap(i, j);
+            } else if (ELE_USED(i) && ELE_USED(j)) {
+                if (ANON_PRIORITY(i) < ANON_PRIORITY(j)) {
+                    callbacks_swap(i, j);
+                }
+            }
+        }
+    }
+}
+
+
+u8 id_by_func(u32 func)
+{
+    for (u8 i = 0; i < ANON_CB_MAX; i++) {
+        if (CB_MASTER[i].func == func)
+            return i;
+    }
+    return 255;
 }
--- src/anonymous_callbacks/anonymous_callbacks.h
+++ src/anonymous_callbacks/anonymous_callbacks.h
@@ -1,9 +1,21 @@
+#ifndef ANONYMOUS_CALLBACKS_H_
+#define ANONYMOUS_CALLBACKS_H_
+
 #include <pokeagb/pokeagb.h>
-#include "../moves/moves.h"
+
+
+#define ANON_CB_MAX 40
+#define ELE_NULL(i) (battle_master->anon_cb_master[i].in_use == false)
+#define ELE_USED(i) (battle_master->anon_cb_master[i].in_use == true)
+#define ANON_PRIORITY(i) battle_master->anon_cb_master[i].priority
+#define CB_MASTER battle_master->anon_cb_master
+#define CB_EXEC_ORDER battle_master->cb_execution_order
+#define CB_EXEC_INDEX battle_master->current_cb_index
 
 #define CB_ON_BEFORE_TURN 1
 #define CB_ON_BEFORE_SWITCH 2
 #define CB_ON_BEFORE_MOVE 3
+#define CB_ON_START 11
 #define CB_ON_MOVE_FAIL 4
 #define CB_ON_MODIFY_MOVE 5
 #define CB_ON_TRYHIT_MOVE 6
@@ -11,7 +23,8 @@
 #define CB_ON_DAMAGE_MOVE 8
 #define CB_ON_EFFECT 9
 #define CB_ON_AFTER_MOVE 10
-#define CB_BEFORE_MOVE 11
+#define CB_ON_TRYHIT_INV_MOVE 12
+
 
 
 struct anonymous_callback {
@@ -20,21 +33,17 @@ struct anonymous_callback {
 	u8 in_use : 1;
 	u8 duration;
 	u8 source_bank; // source bank using the effect
+	u8 cb_id; // execution timing, callback id
 	void* data_ptr;
 	u32 func;
 };
 
-struct anonymous_callbacks_master {
-	struct anonymous_callback before_turn[4];
-	struct anonymous_callback before_switch[4];
-	struct anonymous_callback before_move[4];
-	struct anonymous_callback on_move_fail[4];
-	struct anonymous_callback on_modify_move[4];
-	struct anonymous_callback on_tryhit_move[4];
-	struct anonymous_callback on_base_power_move[4];
-	struct anonymous_callback on_damage_move[4];
-	struct anonymous_callback on_effect_cb[4];
-	struct anonymous_callback on_after_move[4];
-};
+typedef u16 (*AnonymousCallback)(u8 source, u8 target, u16 move, struct anonymous_callback* acb);
 
-typedef u8 (*AnonymousCallback)(u8 source, u8 target, u16 move, struct anonymous_callback* acb);
+extern void add_callback(u8 CB_id, s8 priority, u8 dur, u8 src, u32 func);
+extern void build_execution_order(u8 CB_id);
+extern u16 pop_callback(u8 attacker, u16 move);
+extern void update_callbacks(void);
+extern u8 id_by_func(u32 func);
+
+#endif /* ANONYMOUS_CALLBACKS_H_ */
