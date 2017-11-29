193810a77c7b3fc3c61964d47f4f5e5caf19696a
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2017-11-29 12:58:40 -0500

    seperate rain dmg from weather main damage mod
    Planning to make these into callbacks

--- src/weather/weather.c
+++ src/weather/weather.c
@@ -8,6 +8,35 @@ u8 rain_init_effect(u8 user)
 {
     // if another weather condition is present, remove it TODO
     battle_master->field_state.is_raining = true;
-    add_callback(CB_ON_BEFORE_TURN, 0, 5, user, (u32)NULL);
-    return 5;
+    add_callback(CB_ON_RESIDUAL, 1, 5, NULL); // rain continues to fall, or The rain stopped
+
+}
+
+u16 rain_dmg_mod(u8 user, u16 damage)
+{
+    if ((battle_master->field_state.is_raining) && (B_MOVE_HAS_TYPE(user, MTYPE_WATER))) {
+        return NUM_MOD(damage, 150);
+    } else if (B_MOVE_HAS_TYPE(attacker, MTYPE_FIRE)) {
+        return NUM_MOD(damage, 50);
+    }
+}
+
+u16 sweather_dmg_mod(u16 damage, u8 attacker)
+{
+    if (battle_master->field_state.is_raining || battle_master->field_state.is_primordial_sea) {
+        if (B_MOVE_HAS_TYPE(attacker, MTYPE_WATER)) {
+            return NUM_MOD(damage, 150);
+        } else if (B_MOVE_HAS_TYPE(attacker, MTYPE_FIRE)) {
+            return NUM_MOD(damage, 50);
+        }
+    } else if (battle_master->field_state.is_sunny || battle_master->field_state.is_desolate_land) {
+        if (B_MOVE_HAS_TYPE(attacker, MTYPE_FIRE)) {
+            return NUM_MOD(damage, 150);
+        } else if (B_MOVE_HAS_TYPE(attacker, MTYPE_WATER)) {
+            return NUM_MOD(damage, 50);
+        }
+    } else {
+        return damage;
+    }
+    return damage;
 }
