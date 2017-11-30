6d44b8edd1c747653f254fc3c1bec3835b627a5b
Adrevi <oa.ummels@outlook.com>
2017-11-30 22:49:58 +0100

    Present fully functioning now I think

--- src/moves/present.c
+++ src/moves/present.c
@@ -10,22 +10,20 @@ u8 present_on_modify_move(u8 user, u8 src, u16 move, struct anonymous_callback* 
 
 {
 	if (user != src) return 1;
-	u8 target = TARGET_OF(user);
 	u16 rand_num = rand_range(0, 100);
-	{
-		if (rand_num < 101) {
-			(B_CURRENT_HP(target) + 25);
+	if (rand_num < 20) {
+		if (B_CURRENT_HP(TARGET_OF(user)) == TOTAL_HP(TARGET_OF(user)))
+			return 0;
+		else B_HEAL(TARGET_OF(user)) = 25;
 		}
-		else if (rand_num < 60) {
-			B_MOVE_POWER(user) = 40;
-		}
-		else if (rand_num < 90) {
-			B_MOVE_POWER(user) = 80;
-		}
-		else if (rand_num >= 90) {
-			B_MOVE_POWER(user) = 120;
-		}
-	
+	else if (rand_num < 60) {
+		B_MOVE_POWER(user) = 40;
+	}
+	else if (rand_num < 90) {
+		B_MOVE_POWER(user) = 80;
+	}
+	else if (rand_num >= 90) {
+		B_MOVE_POWER(user) = 120;
 	}
 	return 1;
 }
