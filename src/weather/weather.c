30bacd19e2b2da4762b8552b4b5d8d0545cf9c85
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2017-11-29 20:47:26 -0500

    primordial sea, rain, sunnyday, desolate land

--- src/weather/weather.c
+++ src/weather/weather.c
@@ -3,12 +3,17 @@
 #include "../battle_data/pkmn_bank_stats.h"
 #include "../battle_data/battle_state.h"
 
+void clear_other_weather()
+{
+  // todo
+}
+
 // Rain
-u8 rain_init_effect(u8 user)
+void rain_init_effect()
 {
     // if another weather condition is present, remove it TODO
     battle_master->field_state.is_raining = true;
-    add_callback(CB_ON_RESIDUAL, 1, 5, NULL); // rain continues to fall, or The rain stopped
+    add_callback(CB_ON_RESIDUAL, 1, 5, 0, NULL); // rain continues to fall, or The rain stopped
 
 }
 
@@ -16,27 +21,90 @@ u16 rain_dmg_mod(u8 user, u16 damage)
 {
     if ((battle_master->field_state.is_raining) && (B_MOVE_HAS_TYPE(user, MTYPE_WATER))) {
         return NUM_MOD(damage, 150);
-    } else if (B_MOVE_HAS_TYPE(attacker, MTYPE_FIRE)) {
+    } else if (B_MOVE_HAS_TYPE(user, MTYPE_FIRE)) {
         return NUM_MOD(damage, 50);
+    } else {
+        return damage;
     }
+}
+
+// Primordial sea
+void primordial_sea_init_effect()
+{
+    battle_master->field_state.is_primordial_sea = true;
+    add_callback(CB_ON_RESIDUAL, 1, 5, NULL, NULL);
+}
+
+u16 primordial_sea_dmg_mod(u8 user, u16 damage)
+{
+    // fire type failing handled in tryhit
+    if ((battle_master->field_state.is_primordial_sea) && (B_MOVE_HAS_TYPE(user, MTYPE_WATER))) {
+        return NUM_MOD(damage, 150);
+    } else {
+        return damage;
+    }
+}
+
+u8 primordial_sea_tryhit(u8 user, u8 src, u16 move, struct anonymous_callback* acb)
+{
+    if (B_MOVE_HAS_TYPE(user, MTYPE_FIRE)) {
+        return false;
+    }
+    return true;
+}
+
+// sunny day
+void sunny_day_init_effect()
+{
+    battle_master->field_state.is_sunny = true;
+    add_callback(CB_ON_RESIDUAL, 1, 5, NULL, NULL);
+}
+
+u16 sunny_day_dmg_mod(u8 user, u16 damage)
+{
+    if (B_MOVE_HAS_TYPE(user, MTYPE_FIRE)) {
+        return NUM_MOD(damage, 150);
+    } else if (B_MOVE_HAS_TYPE(user, MTYPE_WATER)) {
+        return NUM_MOD(damage, 50);
+    } else {
+        return damage;
+    }
+}
+
+// desolate land
+void desolate_land_init_effect()
+{
+    battle_master->field_state.is_desolate_land = true;
+    add_callback(CB_ON_RESIDUAL, 1, 5, NULL, NULL);
+}
+
+u16 desolate_land_dmg_mod(u8 user, u16 damage)
+{
+    // fire type failing handled in tryhit
+    if ((battle_master->field_state.is_desolate_land) && (B_MOVE_HAS_TYPE(user, MTYPE_FIRE))) {
+        return NUM_MOD(damage, 150);
+    } else {
+        return damage;
+    }
+}
+
+u8 desolate_land_tryhit(u8 user, u8 src, u16 move, struct anonymous_callback* acb)
+{
+    if (B_MOVE_HAS_TYPE(user, MTYPE_WATER)) {
+        return false;
+    }
+    return true;
+}
+
+// Sandstorm
+void sand_storm_init_effect()
+{
+    battle_master->field_state.is_sand_storm
 }
 
 u16 sweather_dmg_mod(u16 damage, u8 attacker)
 {
-    if (battle_master->field_state.is_raining || battle_master->field_state.is_primordial_sea) {
-        if (B_MOVE_HAS_TYPE(attacker, MTYPE_WATER)) {
-            return NUM_MOD(damage, 150);
-        } else if (B_MOVE_HAS_TYPE(attacker, MTYPE_FIRE)) {
-            return NUM_MOD(damage, 50);
-        }
-    } else if (battle_master->field_state.is_sunny || battle_master->field_state.is_desolate_land) {
-        if (B_MOVE_HAS_TYPE(attacker, MTYPE_FIRE)) {
-            return NUM_MOD(damage, 150);
-        } else if (B_MOVE_HAS_TYPE(attacker, MTYPE_WATER)) {
-            return NUM_MOD(damage, 50);
-        }
-    } else {
-        return damage;
-    }
+
+
     return damage;
 }
