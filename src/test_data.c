7497327058168de8cdef58864c14869b68661b78
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2017-12-06 01:12:35 -0500

    test_data relevant test data moved up
    It's easier for beginners to find the presets now.

--- src/test_data.c
+++ src/test_data.c
@@ -3,63 +3,54 @@
 #include "battle_data/pkmn_bank_stats.h"
 #include "battle_data/battle_state.h"
 
-extern void dprintf(const char * str, ...);
-void set_player_battle_components()
-{
-    /* Change these variables to change what the player's first pokemon has */
-	u16 species = SPECIES_GENGAR;
-    //u16 species = pokemon_getattr(&party_player[0], REQUEST_SPECIES, NULL);
-	u16 move_1 = MOVE_FEINT;
-	u16 move_2 = MOVE_POUND;
-	u16 move_3 = MOVE_PHANTOM_FORCE;
-	u16 move_4 = MOVE_CRAFTY_SHIELD;
-    u8 level = 60;
+/* Player data */
+const static u16 player_species = SPECIES_GARDEVOIR;
+const static u16 player_move_1 = MOVE_GRASSY_TERRAIN;
+const static u16 player_move_2 = MOVE_PSYCHIC_TERRAIN;
+const static u16 player_move_3 = MOVE_ELECTRIC_TERRAIN;
+const static u16 player_move_4 = MOVE_MISTY_TERRAIN;
+const static u8 player_level = 60;
 
-    pokemon_setattr(&party_player[0], REQUEST_SPECIES, &species);
-    pokemon_setattr(&party_player[0], REQUEST_NICK, &pokemon_names[species]);
+/* Opponent data */
+const static u16 opponent_species = SPECIES_CHARMANDER;
+const static u16 opponent_move_1 = MOVE_NATURE_POWER;
+const static u16 opponent_move_2 = MOVE_NATURE_POWER;
+const static u16 opponent_move_3 = MOVE_NATURE_POWER;
+const static u16 opponent_move_4 = MOVE_NATURE_POWER;
+const static u8 opponent_level = 60;
+
+
+
+void set_side_battle_components(u8 side)
+{
+	struct Pokemon* p = (side) ? (&party_player[0]) : (&party_opponent[0]);
+	u16 species = (side) ? (player_species) : (opponent_species);
+	u16 move_1 = (side) ? (player_move_1) : (opponent_move_1);
+	u16 move_2 = (side) ? (player_move_2) : (opponent_move_2);
+	u16 move_3 = (side) ? (player_move_3) : (opponent_move_3);
+	u16 move_4 = (side) ? (player_move_4) : (opponent_move_4);
+	u8 level = (side) ? (player_level) : (opponent_level);
+
+
+    pokemon_setattr(p, REQUEST_SPECIES, &species);
+    pokemon_setattr(p, REQUEST_NICK, &pokemon_names[species]);
     u32 species_exp_index = (pokemon_base_stats[species].exp_growth * 0x194);
     u32 *exp_needed = (u32*) (0x8253AE4 + (species_exp_index + (level * 4)));
-    pokemon_setattr(&party_player[0], REQUEST_EXP_POINTS, exp_needed);
-    recalculate_stats(&party_player[0]);
-    pokemon_setattr(&party_player[0], REQUEST_MOVE1, &move_1);
-    pokemon_setattr(&party_player[0], REQUEST_MOVE2, &move_2);
-    pokemon_setattr(&party_player[0], REQUEST_MOVE3, &move_3);
-    pokemon_setattr(&party_player[0], REQUEST_MOVE4, &move_4);
-    pokemon_setattr(&party_player[0], REQUEST_PP1, &moves[move_1].pp);
-    pokemon_setattr(&party_player[0], REQUEST_PP2, &moves[move_2].pp);
-    pokemon_setattr(&party_player[0], REQUEST_PP3, &moves[move_3].pp);
-    pokemon_setattr(&party_player[0], REQUEST_PP4, &moves[move_4].pp);
-}
-
-void set_opponent_battle_components()
-{
-    /* Change these variables to change what the player's first pokemon has */
-    u16 species = SPECIES_CHARMANDER;
-	u16 move_1 = MOVE_EMBER;
-	u16 move_2 = MOVE_EMBER;
-	u16 move_3 = MOVE_EMBER;
-	u16 move_4 = MOVE_EMBER;
-	u8 level = 40;
-
-    pokemon_setattr(&party_opponent[0], REQUEST_SPECIES, &species);
-    pokemon_setattr(&party_opponent[0], REQUEST_NICK, &pokemon_names[species]);
-    u32 species_exp_index = (pokemon_base_stats[species].exp_growth * 0x194);
-    u32 *exp_needed = (u32*) (0x8253AE4 + (species_exp_index + (level * 4)));
-    pokemon_setattr(&party_opponent[0], REQUEST_EXP_POINTS, exp_needed);
-    recalculate_stats(&party_opponent[0]);
-    pokemon_setattr(&party_opponent[0], REQUEST_MOVE1, &move_1);
-    pokemon_setattr(&party_opponent[0], REQUEST_MOVE2, &move_2);
-    pokemon_setattr(&party_opponent[0], REQUEST_MOVE3, &move_3);
-    pokemon_setattr(&party_opponent[0], REQUEST_MOVE4, &move_4);
-    pokemon_setattr(&party_opponent[0], REQUEST_PP1, &moves[move_1].pp);
-    pokemon_setattr(&party_opponent[0], REQUEST_PP2, &moves[move_2].pp);
-    pokemon_setattr(&party_opponent[0], REQUEST_PP3, &moves[move_3].pp);
-    pokemon_setattr(&party_opponent[0], REQUEST_PP4, &moves[move_4].pp);
+    pokemon_setattr(p, REQUEST_EXP_POINTS, exp_needed);
+    recalculate_stats(p);
+    pokemon_setattr(p, REQUEST_MOVE1, &move_1);
+    pokemon_setattr(p, REQUEST_MOVE2, &move_2);
+    pokemon_setattr(p, REQUEST_MOVE3, &move_3);
+    pokemon_setattr(p, REQUEST_MOVE4, &move_4);
+    pokemon_setattr(p, REQUEST_PP1, &moves[move_1].pp);
+    pokemon_setattr(p, REQUEST_PP2, &moves[move_2].pp);
+    pokemon_setattr(p, REQUEST_PP3, &moves[move_3].pp);
+    pokemon_setattr(p, REQUEST_PP4, &moves[move_4].pp);
 }
 
 
 void test_battle_data()
 {
-  set_player_battle_components();
-  set_opponent_battle_components();
+  set_side_battle_components(0);
+  set_side_battle_components(1);
 }
