b349246f6736e440b126d5a1b47f423eb561ab54
KDSKardabox <bollygame94@gmail.com>
2017-12-07 09:25:05 +0530

    Removed table structures and added hard coded checks for Low Kick etc.

--- src/moves/variable_BP_moves_without_additional_effect.c
+++ src/moves/variable_BP_moves_without_additional_effect.c
@@ -42,39 +42,24 @@ void brine_on_base_power_move(u8 user, u8 src, u16 move, struct anonymous_callba
         B_MOVE_POWER(user) *= 2;
 }
 
-#define NO_OF_REVERSAL_THRESHOLDS 6
-struct basepower_threshold_table reversal_slab[NO_OF_REVERSAL_THRESHOLDS] = {
-                            {20,    66, 96},
-                            {40,    34, 96},
-                            {80,    20, 96},
-                            {100,   10, 96},
-                            {150,   4,  96},
-                            {200,   0,  1},
-                        };
-
 void reversal_on_base_power_move(u8 user, u8 src, u16 move, struct anonymous_callback* acb)
 {
     if (user != src) return;
     u16 current_hp = B_CURRENT_HP(user);
     u16 total_hp = TOTAL_HP(user);
-    for (u8 i=0; i<NO_OF_REVERSAL_THRESHOLDS-1; i++)
-    {
-        if(current_hp >= ((total_hp * reversal_slab[i].numerator)/ reversal_slab[i].denominator))
-        {
-             B_MOVE_POWER(user) = reversal_slab[i].value;
-             break;
-        }
-    }
+    if((current_hp * 48) >= (total_hp * 33))
+        B_MOVE_POWER(user) = 20;
+    else if((current_hp * 48) >= (total_hp * 17))
+        B_MOVE_POWER(user) = 40;
+    else if((current_hp * 48) >= (total_hp * 10))
+        B_MOVE_POWER(user) = 80;
+    else if((current_hp * 48) >= (total_hp * 5))
+        B_MOVE_POWER(user) = 100;
+    else if((current_hp * 48) >= (total_hp * 2))
+        B_MOVE_POWER(user) = 250;
+    else
+        B_MOVE_POWER(user) = 200;
 }
-
-#define NO_OF_HEAVY_SLAM_THRESHOLDS 5
-struct basepower_threshold_table heavy_slam_slab[NO_OF_HEAVY_SLAM_THRESHOLDS] = {
-                            {40,    1, 2},
-                            {60,    1, 3},
-                            {80,    1, 4},
-                            {100,   1, 5},
-                            {120,   0, 1},
-                        };
 
 void heavy_slam_on_base_power_move(u8 user, u8 src, u16 move, struct anonymous_callback* acb)
 {
@@ -82,37 +67,34 @@ void heavy_slam_on_base_power_move(u8 user, u8 src, u16 move, struct anonymous_c
     u8 defender = TARGET_OF(user);
     u16 user_weight = B_WEIGHT(user);
     u16 defender_weight = B_WEIGHT(defender);
-    for (u8 i = 0; i < NO_OF_HEAVY_SLAM_THRESHOLDS - 1; i++)
-    {
-        if((defender_weight * heavy_slam_slab[i].denominator) >= (user_weight * heavy_slam_slab[i].numerator))
-        {
-             B_MOVE_POWER(user) = heavy_slam_slab[i].value;
-             break;
-        }
-    }
-}
+    if((defender_weight * 2) >= user_weight)
+        B_MOVE_POWER(user) = 40;
+    else if((defender_weight * 3) >= user_weight)
+        B_MOVE_POWER(user) = 60;
+    else if((defender_weight * 4) >= user_weight)
+        B_MOVE_POWER(user) = 80;
+    else if((defender_weight * 5) >= user_weight)
+        B_MOVE_POWER(user) = 100;
+    else
+        B_MOVE_POWER(user) = 120;
 
-#define NO_OF_LOW_KICK_THRESHOLDS 6
-struct basepower_threshold_table low_kick_slab[NO_OF_LOW_KICK_THRESHOLDS] = {
-                            {20,    0,   0},
-                            {40,    10,  0},
-                            {60,    25,  0},
-                            {80,    50,  0},
-                            {100,   100, 0},
-                            {120,   200, 0},
-                        };
+}
 
 void low_kick_on_base_power_move(u8 user, u8 src, u16 move, struct anonymous_callback* acb)
 {
     if(user != src) return;
     u8 defender = TARGET_OF(user);
     u16 defender_weight = B_WEIGHT(defender);
-    for (u8 i = NO_OF_HEAVY_SLAM_THRESHOLDS; i > 0; i--)
-    {
-        if(defender_weight  >= low_kick_slab[i].numerator)
-        {
-             B_MOVE_POWER(user) = low_kick_slab[i].value;
-             break;
-        }
-    }
+    if(defender_weight >= 200)
+        B_MOVE_POWER(user) = 120;
+    else if(defender_weight >= 100)
+        B_MOVE_POWER(user) = 100;
+    else if(defender_weight >= 50)
+        B_MOVE_POWER(user) = 80;
+    else if(defender_weight >= 25)
+        B_MOVE_POWER(user) = 60;
+    else if(defender_weight >= 10)
+        B_MOVE_POWER(user) = 40;
+    else
+        B_MOVE_POWER(user) = 20;
 };
