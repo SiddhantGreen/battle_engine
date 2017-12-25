aab4e1ebd2780b2249907d79ff87b32d121d14b3
SiddhantGreen <34700975+SiddhantGreen@users.noreply.github.com>
2017-12-25 15:13:56 +0530

    Update battle_abilities.c

--- src/abilities/battle_abilities.c
+++ src/abilities/battle_abilities.c
@@ -285,12 +285,11 @@ u8 simple_on_stat_boost_mod(u8 user, u8 source, u16 stat_id, struct anonymous_ca
 // Technician
 void technician_on_base_power(u8 user, u8 source, struct anonymous_callback* acb)
 {
-    if (user != source) return true;
+    if (user != source) return;
 	if(B_MOVE_POWER(user) < 60) {
 	   B_MOVE_POWER(user) = NUM_MOD(B_MOVE_POWER(user),150);
-	   return false;
 	}
-	return true;
+	return;
 }
 
 // LEAFGUARD
