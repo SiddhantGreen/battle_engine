57b5d4c4549aaa0a50aae9ac0e910e9657e9f16b
SiddhantGreen <34700975+SiddhantGreen@users.noreply.github.com>
2017-12-27 12:59:59 +0530

    Update moves.h

--- src/moves/moves.h
+++ src/moves/moves.h
@@ -119,6 +119,7 @@ struct move_procs {
 #define IS_STRONG_JAW(move) ((moves[move].m_flags) & FLAG_BITE)
 #define IS_BULLET(move) ((moves[move].m_flags) & FLAG_BULLET)
 #define IS_PULSE(move) ((moves[move].m_flags) & FLAG_PULSE)
+#define IS_RECOIL(move) ((movesdata[move_id].recoil));
 #define IS_SEMI_INVUL(move) ((moves[move].m_flags) & FLAG_SEMI_INVUL)
 #define IS_OHKO(move) ((moves[move].m_flags) & FLAG_OHKO)
 #define CAT_OVERRIDE(move) ((moves[move].m_flags) & FLAG_CATEGORY_OVERRIDE)
